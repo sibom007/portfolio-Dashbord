4c37909a8cbb4725ad6ba648909fb3057073fb8c
sibom007 <vaispark400@gmail.com>
2024-07-03 10:17:05 +0600

    all done

--- src/app/(withDashbordLayout)/dashboard/page.tsx
+++ src/app/(withDashbordLayout)/dashboard/page.tsx
@@ -1,9 +1,37 @@
-const page = () => {
+"use client";
+import { motion, useAnimate } from "framer-motion";
+
+const Mainpage = () => {
+  const boxVariants = {
+    animate: {
+      x: [0, 100, 200, 300, 200, 100, 0, 400, 700, 900, 1000],
+      y: [0, 200, 100, 300, -100, 400, 400, 100, 400, 500],
+      transition: {
+        x: {
+          repeat: Infinity,
+          repeatType: "loop",
+          duration: 5,
+          ease: "easeInOut",
+        },
+        y: {
+          repeat: Infinity,
+          repeatType: "loop",
+          duration: 5,
+          ease: "easeInOut",
+        },
+      },
+    },
+  };
+
   return (
-    <div className="bg-red-600">
-      <h1>page</h1>
+    <div className="flex justify-center">
+      <motion.div
+        variants={boxVariants}
+        animate="animate"
+        initial={{ x: 0 }}
+        className="bg-orange-500 p-20 rounded-2xl flex justify-center items-center"></motion.div>
     </div>
   );
 };
 
-export default page;
+export default Mainpage;
